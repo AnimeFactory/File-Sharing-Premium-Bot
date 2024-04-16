8d681069224ce07807722852303f80c6d79ca447
AnimeFactory <166727007+AnimeFactory@users.noreply.github.com>
2024-04-16 17:45:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from datetime import datetime
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
 
-name ="""By @Madflix_Bots From Telegram"""
+name ="""By @Anime_Factory_Official From Telegram"""
 
 
 class Bot(Client):
@@ -49,7 +49,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support for support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Anime_Factory_123 for support")
                 sys.exit()
 
         try:
@@ -60,18 +60,19 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/MadflixBots_Support for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Anime_Factory_123 for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Madflix_Bots")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Anime_Factory_Official")
         self.LOGGER(__name__).info(f"""
-──╔╦══╦═══╦╗─╔╦╗─╔╦══╗╔═══╦════╦════╗
-──║╠╣╠╣╔═╗║║─║║║─║║╔╗║║╔═╗║╔╗╔╗╠══╗═║
-──║║║║║╚══╣╚═╝║║─║║╚╝╚╣║─║╠╝║║╚╝─╔╝╔╝
-╔╗║║║║╚══╗║╔═╗║║─║║╔═╗║║─║║─║║──╔╝╔╝
-║╚╝╠╣╠╣╚═╝║║─║║╚═╝║╚═╝║╚═╝║─║║─╔╝═╚═╗
-╚══╩══╩═══╩╝─╚╩═══╩═══╩═══╝─╚╝─╚════╝""")
+
+███████╗███████╗███╗░░██╗░█████╗░
+╚════██║██╔════╝████╗░██║██╔══██╗
+░░███╔═╝█████╗░░██╔██╗██║██║░░██║
+██╔══╝░░██╔══╝░░██║╚████║██║░░██║
+███████╗███████╗██║░╚███║╚█████╔╝
+╚══════╝╚══════╝╚═╝░░╚══╝░╚════╝░""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
