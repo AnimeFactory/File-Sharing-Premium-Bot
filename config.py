d34fd4012e560835afe7d78787194629a505f18a
AnimeFactory <166727007+AnimeFactory@users.noreply.github.com>
2024-05-08 14:58:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,19 +14,19 @@ from logging.handlers import RotatingFileHandler
 
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7115826141:AAHyK12NjRqdn_YSMgw0wr9z7lEZiddsUqE")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7098250905:AAF0bUJJoAGl2bocPDUWARIN5ZvkapRRNHo")
 APP_ID = int(os.environ.get("APP_ID", "21016163"))
 API_HASH = os.environ.get("API_HASH", "365f3421c4243310e1c738e2b6fb63a1")
 
 
-OWNER = os.environ.get("OWNER", "https://t.me/Grand_Zeno_Omni_KingBot") #Owner username
+OWNER = os.environ.get("OWNER", "https://t.me/Jack_AFO") #Owner username
 OWNER_ID = int(os.environ.get("OWNER_ID", "5806054139")) #Owner user id
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://frankanimes:qayplmwsxokn@cluster0.xksjqdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002042077879"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002122508523"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002063266681"))
 
 
 SECONDS = int(os.getenv("SECONDS", "600")) # auto delete in seconds
@@ -39,7 +39,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "2"))
 
 
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI am a file store bot Powered by @Anime_Factory_Official ⚡.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}✨\n\nI am a file store bot Powered by @Anime_Carnival_4U ⚡.")
 
 try:
     ADMINS=[5806054139]
@@ -49,11 +49,11 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}✨\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot !"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(7085541484)
+ADMINS.append(5806054139)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
